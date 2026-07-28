16bbbb813590d1dbccdd9755090e4dd65175e2aa
AcharaChisomSolomon <acharachisom861@gmail.com>
2026-07-28 09:23:52 +0100

    Project structure refactored

--- models/note.js
+++ models/note.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-mongoose.connect(url, { family: 4 })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log(`error connecting to MongoDB: ${error.message}`)
-    })
-
 const noteSchema = new mongoose.Schema({
     content: {
         type: String,
@@ -20,6 +8,7 @@ const noteSchema = new mongoose.Schema({
     },
     important: Boolean,
 })
+
 noteSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
@@ -27,4 +16,5 @@ noteSchema.set('toJSON', {
         delete returnedObject.__v
     }
 })
+
 module.exports = mongoose.model('Note', noteSchema)
